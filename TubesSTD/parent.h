aec2c7f16faeca0cd6c45b64246ad403ece8d076
fadhilwj <m.fadhillah.w@gmail.com>
2024-12-21 04:40:18 +0700

    Update parent.h

--- TubesSTD/parent.h
+++ TubesSTD/parent.h
@@ -1,45 +1,37 @@
 #ifndef parent_H_INCLUDED
 #define parent_H_INCLUDED
+#include <iostream>
 
-#define prevJ(j) (j)->prevJ
 #define infoJ(j) (j)->infoJ
-#define nextB(j) (j)->nextB
 #define nextJ(j) (j)->nextJ
 #define firstJ(LJ) (LJ).firstJ
-#define lastJ(LJ) (LJ).lastJ
-
-#include "child.h"
 
 using namespace std;
 
 typedef string infotypeJ;
 typedef struct elmJenis *adrJenis;
 
-struct elmJenis
-{
-    adrJenis prevJ;
-    infotype infoJ;
-    ListB lB;
-    adrJenis nextJ;
-};
 struct ListJ
 {
-    adrJenis firstB;
-    adrJenis lastB;
+    adrJenis firstJ;
+};
+
+struct elmJenis
+{
+    infotypeJ infoJ;
+    adrJenis nextJ;
 };
 
 void createListJenis(ListJ &LJ);
-adrJenis createElemenJenis(adrJenis j, infotypeJ x);
+adrJenis createElemenJenis(adrJenis j, infotypeJ jenis);
 bool isEmptyJ(ListJ LJ);
 void insertLastJenis(ListJ &LJ, adrJenis j);
 void showJenis(ListJ LJ);
 adrJenis searchJenis(ListJ LJ, string jenis);
 void deleteFirstJenis(ListJ &LJ, adrJenis &j);
 void deleteLastJenis(ListJ &LJ, adrJenis &j);
-void deleteAfterJenis(ListJ &LJ, adrJenis &j);
-void deleteJenisByNama(ListJ &LJ, string jenis, adrJenis &j);
-void showJenisNBiaya(ListJ LJ, ListB LB);
-int totalBiaya(ListJ LJ);
+void deleteAfterJenis(ListJ &LJ, adrJenis prec, adrJenis &j);
+
 
 
 #endif // parent_H_INCLUDED
