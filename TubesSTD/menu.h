6fbf5ac416a666c1a97e074c41eff934ce770129
fadhilwj <m.fadhillah.w@gmail.com>
2024-12-21 04:42:42 +0700

    Update menu.h

--- TubesSTD/menu.h
+++ TubesSTD/menu.h
@@ -1,1 +1,14 @@
+#ifndef MENU_H
+#define MENU_H
 
+#include <iostream>
+#include <string>
+#include "parent.h"
+
+using namespace std;
+
+int displayMenu();
+string displayUpdateBiaya();
+string displayHapusBiaya();
+
+#endif
